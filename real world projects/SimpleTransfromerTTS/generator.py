00efa0ec4e52bd7d7971275ac440fbb647a47278
ValenO1 <108626943+ValenO1@users.noreply.github.com>
2024-04-18 07:52:27 +0200

    Add files via upload

---
+++ real world projects/SimpleTransfromerTTS/generator.py
@@ -0,0 +1,40 @@
+import IPython
+import torch
+
+from model import TransformerTTS
+from melspecs import inverse_mel_spec_to_wav
+from write_mp3 import write_mp3
+from hyperparams import hp
+from text_to_seq import text_to_seq
+import matplotlib.pyplot as plt
+
+train_saved_path = r"E:\Grad Project\SimpleTransfromerTTS-master\train_SimpleTransfromerTTS.pt"
+state = torch.load(train_saved_path)
+model = TransformerTTS().cuda()
+model.load_state_dict(state["model"])
+
+text = "hi i am ahmed rady and i want to fuck elsam mohsen so hard from his ass because he is so sexy"
+name_file = "output.mp3"
+
+
+postnet_mel, gate = model.inference(
+  text_to_seq(text).unsqueeze(0).cuda(),
+  stop_token_threshold=1e-4,
+  with_tqdm = False
+)
+
+audio = inverse_mel_spec_to_wav(postnet_mel.detach()[0].T)
+
+plt.plot(
+    torch.sigmoid(gate[0, :]).detach().cpu().numpy()
+)
+
+write_mp3(
+    audio.detach().cpu().numpy(),
+    name_file
+)
+
+IPython.display.Audio(
+    audio.detach().cpu().numpy(),
+    rate=hp.sr
+)
